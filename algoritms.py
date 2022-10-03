99398960049698b26e41403d15ea6795b4d71a25
shamin daniil <danya.shamin.06@mail.ru>
2022-10-03 21:01:41 +0300

    add all algoritms and prepare project to use

--- algoritms.py
+++ algoritms.py
@@ -1,3 +1,4 @@
+from math import degrees
 from constants import *
 
 class Card():
@@ -34,7 +35,7 @@ def ray_casting(player, card_cur):
             depth_h = (y-oy)/sin_cur
             xh = ox+depth_h*cos_cur
             if mapping(xh, y+dy) in card_cur.dict:
-                letter_h = card_cur.dict[mapping(x+dx, yv)]
+                letter_h = card_cur.dict[mapping(xh, y+dy)]
                 break
             y+=dy*TILE
         depth, letter, offset = (depth_v, letter_v, yv) if depth_v<depth_h else (depth_h, letter_h, xh)
@@ -46,6 +47,44 @@ def ray_casting(player, card_cur):
         walls.append((depth, wall_collumn, (ray*SCREEN_SCALE, SCREEN_HEIGHT/2-proj_height/2)))
         cur_angle+=DELTA_ANGLE
     return walls
+
+class SpriteObject():
+    def __init__(self, sprite_type, static, pos, shift, scale):
+        self.image = TEXTURES_SPRITES[sprite_type]
+        self.static = static
+        self.pos = self.x, self.y = pos
+        self.shift = shift
+        self.scale = scale
+        if not self.static:
+            self.sprite_angles = [frozenset(range(i, i+45)) for i in range(0, 360, 45)]
+            self.sprite_images = {angles:image for angles, image in zip(self.sprite_angles, self.image)}
+    def object_located(self, player, walls):
+        dx, dy = self.x-player.x, self.y-player.y
+        theta = atan2(dy, dx)
+        gamma = theta - player.angle
+        if dx>0 and 180<=degrees(player.angle)<=360 or dx<0 and dy<0:
+            gamma+=DOUBLE_PI
+        delta_rays = int(gamma/DELTA_ANGLE)
+        current_ray = CENTER_RAY+delta_rays
+        if 0<=current_ray<=FAKE_RAYS_RANGE:
+            if not self.static:
+                if theta<0:
+                    theta+=DOUBLE_PI
+                theta = 360 - int(degrees(theta))
+                for angles in self.sprite_angles:
+                    if theta in angles:
+                        self.image = self.sprite_images[angles]
+                        break
+            distance_to_sprite = sqrt(dx**2+dy**2)
+            distance_to_sprite*=cos(HALF_FOV - current_ray*DELTA_ANGLE)
+            proj_height = min(int(PROJ_COEF/distance_to_sprite), 2*SCREEN_HEIGHT)
+            half_proj_height = proj_height//2
+            sprite = pg.transform.scale(self.image, (proj_height, proj_height))
+            shift = self.shift*half_proj_height
+            sprite_pos = (current_ray*SCREEN_SCALE-half_proj_height, SCREEN_HEIGHT/2-proj_height/2+shift)
+            return (distance_to_sprite, sprite, sprite_pos)
+        else:
+            return (False,)
 
 class Player():
     def __init__(self, x, y, angle, speed):
@@ -59,6 +98,27 @@ class Player():
                 self.x+=cos(self.angle)*self.speed
             if not (mapping(self.x, self.y+sin(self.angle)*self.speed) in card_cur.dict):
                 self.y+=sin(self.angle)*self.speed
+        if self.backw:
+            if not (mapping(self.x-cos(self.angle)*self.speed, self.y) in card_cur.dict):
+                self.x-=cos(self.angle)*self.speed
+            if not (mapping(self.x, self.y-sin(self.angle)*self.speed) in card_cur.dict):
+                self.y-=sin(self.angle)*self.speed
+        if self.rightw:
+            if not (mapping(self.x-sin(self.angle)*self.speed, self.y) in card_cur.dict):
+                self.x-=sin(self.angle)*self.speed
+            if not (mapping(self.x, self.y+cos(self.angle)*self.speed) in card_cur.dict):
+                self.y+=cos(self.angle)*self.speed
+        if self.leftw:
+            if not (mapping(self.x+sin(self.angle)*self.speed, self.y) in card_cur.dict):
+                self.x+=sin(self.angle)*self.speed
+            if not (mapping(self.x, self.y-cos(self.angle)*self.speed) in card_cur.dict):
+                self.y-=cos(self.angle)*self.speed
+        if self.rightp:
+            self.angle+=0.02
+            self.angle%=DOUBLE_PI
+        if self.leftp:
+            self.angle-=0.02
+            self.angle%=DOUBLE_PI
     def drawing(self, card_cur, running):
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -66,15 +126,39 @@ class Player():
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_w:
                     self.forw = True
+                if event.key == pg.K_s:
+                    self.backw = True
+                if event.key == pg.K_d:
+                    self.rightw = True
+                if event.key == pg.K_a:
+                    self.leftw = True
+                if event.key == pg.K_RIGHT:
+                    self.rightp = True
+                if event.key == pg.K_LEFT:
+                    self.leftp = True
             if event.type == pg.KEYUP:
                 if event.key == pg.K_w:
                     self.forw = False
+                if event.key == pg.K_s:
+                    self.backw = False
+                if event.key == pg.K_d:
+                    self.rightw = False
+                if event.key == pg.K_a:
+                    self.leftw = False
+                if event.key == pg.K_RIGHT:
+                    self.rightp = False
+                if event.key == pg.K_LEFT:
+                    self.leftp = False
+        SCREEN.fill(COLORS['WHITE'])
         self.management(card_cur)
         walls = ray_casting(self, card_cur)
-        objs = walls
-        for obj in sorted(walls, key=lambda n:n[0], reverse=True):
+        sprites = [sprite.object_located(self, walls) for sprite in sprites_on_card]
+        objs = walls+sprites
+        for obj in sorted(objs, key=lambda n:n[0], reverse=True):
             if obj[0]:
                 distance, image, pos = obj
                 SCREEN.blit(image, pos)
         pg.display.update()
         return running
+    
+sprites_on_card = [SpriteObject('SPRITE_TRIAL', False, (200, 200), 1, 1)]
--- constants.py
+++ constants.py
@@ -1,11 +1,15 @@
 import pygame as pg
 pg.init()
-from math import sin, cos, tan, atan2, pi
+from math import sin, cos, tan, atan2, pi, sqrt
 
 TILE = 100
 NUM_RAYS = 300
+CENTER_RAY = NUM_RAYS//2-1
+FAKE_RAYS = 100
+FAKE_RAYS_RANGE = FAKE_RAYS+NUM_RAYS+FAKE_RAYS
 FOV = pi/3
 HALF_FOV = FOV/2
+DOUBLE_PI = pi*2
 DELTA_ANGLE = FOV/NUM_RAYS
 DIST = NUM_RAYS/(2*tan(HALF_FOV))
 PROJ_COEF = 3*DIST*TILE
@@ -14,7 +18,9 @@ SCREEN_HEIGHT = 800
 SCREEN_SCALE = SCREEN_WIDTH/NUM_RAYS
 SCREEN = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 TEXTURES_WALLS = {'1':pg.image.load('game_files\TEXTURES\TEXTURES_WALLS\WALL_TRIAL.bmp').convert()}
+TEXTURES_SPRITES = {'SPRITE_TRIAL':[pg.image.load(f'game_files\TEXTURES\TEXTURES_SPRITES\SPRITE_TRIAL\SPRITE_TRIAL.{i}.bmp') for i in range(8)]}
 CARDS = {"CARD_TRIAL":open('game_files\CARDS\CARD_TRIAL.txt')}
+COLORS = {'WHITE':(255, 255, 255)}
 TEXTURES_WIDTH = 1200
 TEXTURES_HEIGHT = 1200
 TEXTURES_SCALE = TEXTURES_WIDTH/TILE
